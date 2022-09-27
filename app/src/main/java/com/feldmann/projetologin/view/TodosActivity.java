debc8ef16cb82596c192e1fea9c7a7113c5b87e7
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-09-27 20:52:24 -0300

    criado activity para todos

---
+++ app/src/main/java/com/feldmann/projetologin/view/TodosActivity.java
@@ -0,0 +1,16 @@
+package com.feldmann.projetologin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.feldmann.projetologin.R;
+
+public class TodosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todos);
+    }
+}
