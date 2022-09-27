ad7d9d4d3b984ac272f815b5d74cd6ee3a732a90
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-09-27 12:50:34 -0300

    criado classe e xml de cadastro

---
+++ app/src/main/java/com/feldmann/projetologin/view/CadastroActivity.java
@@ -0,0 +1,14 @@
+package com.feldmann.projetologin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.feldmann.projetologin.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
