e44be428a8151084376a081928f806d79d272f38
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-09-27 20:52:58 -0300

    criado activity para user

---
+++ app/src/main/java/com/feldmann/projetologin/view/UserActivity.java
@@ -0,0 +1,16 @@
+package com.feldmann.projetologin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.feldmann.projetologin.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
