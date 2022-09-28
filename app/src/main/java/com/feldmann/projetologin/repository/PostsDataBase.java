8c8a5c63d0b22963774ddd33a30db216a34ed585
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-09-28 08:19:00 -0300

    criado construtor, gets e sets para posts

--- app/src/main/java/com/feldmann/projetologin/repository/PostsDataBase.java
+++ app/src/main/java/com/feldmann/projetologin/repository/PostsDataBase.java
@@ -1,4 +1,16 @@
 package com.feldmann.projetologin.repository;
 
+import android.content.Context;
+import com.feldmann.projetologin.model.Posts;
+import java.util.List;
+
 public class PostsDataBase {
+    private static final String tagLog = "PostsDB";
+    //
+    private static List<Posts> posts;
+    private static UserDataBase instance = null;
+    //
+    private PostsDataBase(Context context){
+        super();
+    }
 }
